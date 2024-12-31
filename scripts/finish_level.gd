07212c7cc8d1004262f543250f0d017d1b937dea
cat <kittynekostudio@gmail.com>
2024-12-31 09:05:50 -0800

    Added moving between levels. Added basic level to inherit from.

--- scripts/finish_level.gd
+++ scripts/finish_level.gd
@@ -3,3 +3,4 @@ extends Area3D
 
 func _on_body_entered(_body: Node3D) -> void:
 	SignalBus.finish_level.emit()
+	get_parent().queue_free()
--- scripts/main.gd
+++ scripts/main.gd
@@ -1,43 +1,51 @@
 extends Node
 
 var level_1: PackedScene = preload("res://scenes/level_1.tscn")
+var level_2: PackedScene = preload("res://scenes/level_2.tscn")
 var death_screen: PackedScene = preload("res://scenes/death_screen.tscn")
-var current_level: String
+var current_level: int = 0
 
 var completion_times: Dictionary
-var level_number: int = 1
 
 
 func _ready() -> void:
 	$Timer.set_process(false)
 	SignalBus.finish_level.connect(finish_level)
-	SignalBus.connect("player_died", player_died)
-	SignalBus.connect("restart_level", restart_level)
+	SignalBus.player_died.connect(player_died)
+	SignalBus.restart_level.connect(restart_level)
 	$TitleScreen/StartGame.pressed.connect(start_game)
 
 
 func start_game() -> void:
 	$TitleScreen.queue_free()
-	var level_1_instance: Node = level_1.instantiate()
-	add_child(level_1_instance)
-	current_level = "level_1"
+	current_level += 1
+	load_level()
 	$Timer.set_process(true)
 
 
 func finish_level() -> void:
-	completion_times["level_" + str(level_number)] = $Timer.reset()
-	level_number += 1
+	completion_times["level_" + str(current_level)] = $Timer.reset()
+	$Timer.set_process(false)
+	current_level += 1
 	if completion_times.has("level_1"):
 		print(completion_times)
-
+	load_level()
+	$Timer.set_process(true)
 
 func restart_level() -> void:
-	match current_level:
-		"level_1":
-			var level_1_instance: Node = level_1.instantiate()
-			add_child(level_1_instance)
+	load_level()
 	
 	$Timer.set_process(true)
+
+
+func load_level() -> void:
+	match current_level:
+		1:
+			var level_1_instance: Node = level_1.instantiate()
+			add_child(level_1_instance)
+		2:
+			var level_2_instance: Node = level_2.instantiate()
+			add_child(level_2_instance)
 
 
 func player_died() -> void:
